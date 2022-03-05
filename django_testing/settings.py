00796518d6a371614af3592bd25a0af5a494ac9c
thrashches <thrashches@yandex.ru>
2022-03-05 20:49:26 +0300

    Retrieve update delete

--- django_testing/settings.py
+++ django_testing/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'django_testing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3'
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_django_testing',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'netology_django_testing',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
 }
 
 
--- tests/students/test_courses_api.py
+++ tests/students/test_courses_api.py
@@ -41,22 +41,34 @@ class TestCourseModel:
                    'name'] == random_name, f'Course name must be {random_name}, not {response.data["name"]}'
 
     @pytest.mark.django_db(transaction=True)
-    def test_course_create_update_delete(self):
+    def test_course_create(self):
         fixture_path = os.path.join(settings.BASE_DIR, 'tests', 'fixtures', 'courses.json')
         with open(fixture_path, 'r', encoding='utf-8') as f:
             data = json.load(f)
         create_response = client.post('/api/v1/courses/', data=data[0])
-        course_id = create_response.data['id']
+
         assert create_response.status_code == 201, f'Status code {create_response.status_code} from ' \
                                                    f'url: \'/api/v1/courses/\''
 
-        update_response = client.put(f'/api/v1/courses/{course_id}/', data=data[1])
+    @pytest.mark.django_db(transaction=True)
+    def test_course_update(self, course):
+        original_response = client.get(f'/api/v1/courses/{course.id}/')
+        assert original_response.status_code == 200, f'Status code {original_response.status_code} from ' \
+                                                     f'url: \'/api/v1/courses/{course.id}\''
+        update_response = client.put(f'/api/v1/courses/{course.id}/', data={'name': 'New name'})
         assert update_response.status_code == 200, f'Status code {update_response.status_code} from ' \
-                                                   f'url: \'/api/v1/courses/{course_id}\''
-        retrieve_response = client.get(f'/api/v1/courses/{course_id}/')
-        assert retrieve_response.status_code == 200, f'Status code {retrieve_response.status_code} from ' \
-                                                     f'url: \'/api/v1/courses/{course_id}\''
-        assert retrieve_response.data['name'] == data[1]['name'], 'Update failed!'
+                                                   f'url: \'/api/v1/courses/{course.id}\''
+        updated_response = client.get(f'/api/v1/courses/{course.id}/')
+        assert updated_response.data != original_response.data, 'New data is the same as original!'
 
-        delete_response = client.delete(f'/api/v1/courses/{course_id}/')
-        assert delete_response.status_code == 204, f'Delete failed! Status code: {delete_response.status_code}'
+    @pytest.mark.django_db(transaction=True)
+    def test_delete_course(self, course):
+        retrieve_response = client.get(f'/api/v1/courses/{course.id}/')
+        assert retrieve_response.status_code == 200, f'Status code {retrieve_response.status_code} from ' \
+                                                     f'url: \'/api/v1/courses/{course.id}\''
+        delete_response = client.delete(f'/api/v1/courses/{course.id}/')
+        assert delete_response.status_code == 204, f'Status code {retrieve_response.status_code} from ' \
+                                                   f'url: \'/api/v1/courses/{course.id}\''
+        check_delete_response = client.get(f'/api/v1/courses/{course.id}/')
+        assert check_delete_response.status_code == 404, f'Status code {check_delete_response.status_code} from ' \
+                                                         f'url: \'/api/v1/courses/{course.id}\''
